5894a01464430db2c0c0833bd452b365c4756be7
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-07-28 23:48:37 +0100

    basic website starter

--- react-starter/client/src/App.js
+++ react-starter/client/src/App.js
@@ -1,88 +1,14 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React from "react";
+import MainNav from './navigation/MainNav';
 
-import "./App.css";
-
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, update: 0};
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  update = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(this.state.update).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  handleInput = event => {
-    this.setState({ update: event.target.value });
-  }
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>First Full Stack Truffle App</h1>
-        <div>
-          <p>Enter a number into the box below and hit submit!</p>
+function App(){
+    return(
+        <div align="center">
+            <h1>Welcome to my portfolio website</h1>
+            <MainNav />
+            <a href="https://github.com/marronjo">Github</a>
         </div>
-        <div className="value">The stored value is:  {this.state.storageValue}</div>
-        <div>
-          <input className="input" onChange={this.handleInput} placeholder="Enter Number"/>
-          <button className="button button1"onClick={this.update}>Submit</button>
-        </div>
-      </div>
     );
-  }
 }
 
 export default App;
---
+++ react-starter/client/src/navigation/MainNav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function MainNav(){
+    return(
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            First Project
+                        </li>
+                        <li>Compound Finance Tracker</li>
+                        <li>DEX Project</li>
+                    </ul>
+                </nav>
+            </header>
+    );
+}
+
+export default MainNav;
